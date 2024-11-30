e3a5aa5df8d7f7d88f3eef49640830a29b9320ff
Peter Dinis <peterdinis0@pobox.sk>
2024-11-30 20:09:00 +0100

    feat: Working on landing page WIP

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [IconsModule],
+  imports: [IconsModule, ButtonModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
 }

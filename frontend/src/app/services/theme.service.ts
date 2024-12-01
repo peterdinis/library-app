ded656a928ca12abd12ba55fb5a1a567a3251884
Peter Dinis <peterdinis0@pobox.sk>
2024-12-01 13:57:08 +0100

    feat: Adding support logic for dark/light mode

--- frontend/src/app/services/theme.service.ts
+++ frontend/src/app/services/theme.service.ts
@@ -7,28 +7,39 @@ export class ThemeService {
   private darkModeClass = 'dark';
 
   constructor() {
-    // Apply the saved theme on initialization
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-      document.documentElement.classList.add(savedTheme);
+    if (this.isBrowser()) {
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme) {
+        document.documentElement.classList.add(savedTheme);
+      }
     }
   }
 
   enableDarkMode(): void {
-    document.documentElement.classList.add(this.darkModeClass);
-    localStorage.setItem('theme', this.darkModeClass);
+    if (this.isBrowser()) {
+      document.documentElement.classList.add(this.darkModeClass);
+      localStorage.setItem('theme', this.darkModeClass);
+    }
   }
 
   disableDarkMode(): void {
-    document.documentElement.classList.remove(this.darkModeClass);
-    localStorage.removeItem('theme');
+    if (this.isBrowser()) {
+      document.documentElement.classList.remove(this.darkModeClass);
+      localStorage.removeItem('theme');
+    }
   }
 
   toggleTheme(): void {
-    if (document.documentElement.classList.contains(this.darkModeClass)) {
-      this.disableDarkMode();
-    } else {
-      this.enableDarkMode();
+    if (this.isBrowser()) {
+      if (document.documentElement.classList.contains(this.darkModeClass)) {
+        this.disableDarkMode();
+      } else {
+        this.enableDarkMode();
+      }
     }
+  }
+
+  private isBrowser(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
   }
 }

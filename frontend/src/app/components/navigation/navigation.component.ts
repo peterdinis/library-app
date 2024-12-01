3f8c73ec223a5f01c396f77d5361a4dc904e08c9
Peter Dinis <peterdinis0@pobox.sk>
2024-12-01 13:54:27 +0100

    FOO

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
+import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-navigation',
-  imports: [CommonModule, IconsModule],
+  imports: [CommonModule, IconsModule, ThemeToggleComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })

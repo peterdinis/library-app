8bbdf68bfc35d470d43328fb2944e9e8bcd9f69a
Peter Dinis <peterdinis0@pobox.sk>
2024-12-08 18:08:24 +0100

    FOO

--- src/lib/components/books/BooksWrapper.svelte
+++ src/lib/components/books/BooksWrapper.svelte
@@ -1,3 +1,11 @@
+<script>
+
+  import BookSearch from "./BookSearch.svelte";
+
+</script>
 <div class="flex justify-center items-center">
 	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-5">Všetky knihy</h1>
+	<div class="mt-5">
+		<BookSearch />
+	</div>
 </div>	

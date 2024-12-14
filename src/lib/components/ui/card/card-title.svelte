f03db9309dd026825641c55ff96f0612e0a09e3a
Peter Dinis <peterdinis0@pobox.sk>
2024-12-14 11:15:55 +0100

    infra: Install card component

--- src/lib/components/ui/card/card-title.svelte
+++ src/lib/components/ui/card/card-title.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-import { cn } from "$lib/utils.js";
-import type { HTMLAttributes } from "svelte/elements";
-import type { HeadingLevel } from "./index.js";
+	import type { HTMLAttributes } from "svelte/elements";
+	import type { HeadingLevel } from "./index.js";
+	import { cn } from "$lib/utils.js";
 
-type $$Props = HTMLAttributes<HTMLHeadingElement> & {
-	tag?: HeadingLevel;
-};
+	type $$Props = HTMLAttributes<HTMLHeadingElement> & {
+		tag?: HeadingLevel;
+	};
 
-const className: $$Props["class"] = undefined;
-export const tag: $$Props["tag"] = "h3";
-export { className as class };
+	let className: $$Props["class"] = undefined;
+	export let tag: $$Props["tag"] = "h3";
+	export { className as class };
 </script>
 
 <svelte:element
 	this={tag}
-	class={cn("font-semibold leading-none tracking-tight", className)}
+	class={cn("text-lg font-semibold leading-none tracking-tight", className)}
 	{...$$restProps}
 >
 	<slot />

87ab2184ca5cff238f20d8159b0296809f4e143f
Peter Dinis <peterdinis0@pobox.sk>
2024-12-14 10:41:15 +0100

    feat: Design books page WIP

--- src/lib/components/books/BooksWrapper.svelte
+++ src/lib/components/books/BooksWrapper.svelte
@@ -5,7 +5,8 @@
 </script>
 <div class="flex justify-center items-center">
 	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-5">Všetky knihy</h1>
-	<div class="mt-5">
-		<BookSearch />
-	</div>
 </div>	
+
+<div class="mt-5 flex justify-center items-center">
+	<BookSearch />
+</div>

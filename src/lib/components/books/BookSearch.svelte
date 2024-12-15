3a035d72b31c5ec87e0fda1162bf813488843190
Peter Dinis <peterdinis0@pobox.sk>
2024-12-15 15:56:57 +0100

    infra: Updating translations WIP

--- src/lib/components/books/BookSearch.svelte
+++ src/lib/components/books/BookSearch.svelte
@@ -14,15 +14,15 @@
     </svg>
 
     <input
-      class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-      placeholder="Search for book.."
+      class="w-full bg-transparent placeholder:text-slate-400 dark:placeholder:text-white text-slate-700 dark:text-slate-50 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+      placeholder="Hľadať knihu..."
     />
 
     <button
       class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
       type="button"
     >
-      Search
+      Hľadaj
     </button>
   </div>
 </div>

34b95b53045e3cce418a01e9d9b7172b900afb86
Peter Dinis <peterdinis0@pobox.sk>
2024-12-15 15:17:28 +0100

    FOO

--- src/lib/components/books/BookSearch.svelte
+++ src/lib/components/books/BookSearch.svelte
@@ -15,7 +15,7 @@
 
     <input
       class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-      placeholder="UI Kits, Dashboards..."
+      placeholder="Search for book.."
     />
 
     <button
